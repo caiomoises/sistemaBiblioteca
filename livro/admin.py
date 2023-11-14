5bb75bc232dc2727edcd743d3881a76ff15f2236
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-11-14 15:51:26 -0300

    feat: added, search book

--- livro/admin.py
+++ livro/admin.py
@@ -4,3 +4,5 @@ from .models import *
 @admin.register(livro)
 class livroAdmin(admin.ModelAdmin):
     list_display = ('Nome', 'Idioma', 'Codigo', 'Edição', 'Quantidade')
+    search_fields = 'Nome', 'Codigo'
+    list_per_page = 10
--- livro/models.py
+++ livro/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.forms import SelectDateWidget
 
 class livro(models.Model):
     idiomas = [
